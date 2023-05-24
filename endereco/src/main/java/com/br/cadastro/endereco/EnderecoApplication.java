8c3bd69c42bf658fa021d97e7d4ca06ea61b39f0
lucarauj <lukas.martins193@gmail.com>
2023-05-24 11:55:06 -0300

    Add configuracao de Build da API

--- endereco/src/main/java/com/br/cadastro/endereco/EnderecoApplication.java
+++ endereco/src/main/java/com/br/cadastro/endereco/EnderecoApplication.java
@@ -2,11 +2,18 @@ package com.br.cadastro.endereco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-public class EnderecoApplication {
+public class EnderecoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EnderecoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EnderecoApplication.class, args);
